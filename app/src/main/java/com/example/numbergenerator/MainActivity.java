e468210745dda7cc1612d8b7690e8f2e25ff7e4e
SHARANHS <sharanphineas@gmail.com>
2019-05-23 00:16:45 +0530

    Text Views Added and Main Activity Completed

--- app/src/main/java/com/example/numbergenerator/MainActivity.java
+++ app/src/main/java/com/example/numbergenerator/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.numbergenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollbutton = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.numberTextView);
+        final SeekBar seekBar = findViewById(R.id.seekBar2);
+
+        rollbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int a=rand.nextInt(seekBar.getProgress())+1;
+                textView.setText(Integer.toString(a));
+            }
+        }
+
+        );
+
     }
 
 }
